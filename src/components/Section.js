95b4e4f0a3553f7ab766c1bb1b786231ff02efc5
Mersant <dlowry99@outlook.com>
2022-10-09 15:26:18 -0400

    Finished projects page, added accessibility features

--- src/components/Section.js
+++ src/components/Section.js
@@ -8,7 +8,7 @@ function Section() {
     <section className="section">
       <div id='buttonsContainer'>
         <button>My projects</button>
-        <button>Resume</button>
+        <a href='https://drive.google.com/file/d/1_f93M3E2ubnvJTY2tfUcPI0rUwsrfQnV/view?usp=sharing' target="_blank" rel="noopener noreferrer" download={true}><button>Resume</button></a>
         <button>Get in touch</button>
       </div>
       
@@ -33,10 +33,15 @@ function Section() {
       RET<br/>
       </p>
       <div id='pageContent'>
-        <h2 id='pageTitle'>My Projects</h2>
 
+        <h2 id='pageTitle'>My Projects</h2>
+        <p>
+          As I make my foray into the programming industry, I hope these projects can help you see some of my potential.
+        </p>
         <div className='projectHeader'>
-        <img className='projectImage' src='https://github.com/meyerbw10/Find-My-Pets/blob/main/public/images/photo1.png?raw=true'/>
+          <a class='projectLink' href="https://github.com/meyerbw10/Find-My-Pets" target="_blank" rel="noopener noreferrer">
+            <img className='projectImage' src='https://github.com/meyerbw10/Find-My-Pets/blob/main/public/images/photo1.png?raw=true' alt="PawFinder's homepage, showing a happy, young man petting his golden retriever on a grass field."/>
+          </a>
         <div className='projectInfo'>
           <h2>PawFinder</h2>
           <p>
@@ -44,45 +49,62 @@ function Section() {
           </p>
         </div>
         </div>
-        <a class='projectLink' href="https://www.google.com/" target="_blank" rel="noopener noreferrer">See this project on GitHub</a>
+        <a class='projectLink' href="https://github.com/meyerbw10/Find-My-Pets" target="_blank" rel="noopener noreferrer">See this project on GitHub</a>
 
 
         <div className='projectHeader'>
+        <div className='projectInfo'>
+          <h2>Proposal Game</h2>
+          <p>
+            At the beginning of 2022 I began work on a video game to propose to my then girlfriend of three years. After a several months of work, I proposed on May 29th and she said, "Yes!" 
+          </p>
+        </div>
+        <a class='projectLink' href="https://github.com/Mersant/redesigned-barnacle" target="_blank" rel="noopener noreferrer">
+          <img className='projectImage' src='https://user-images.githubusercontent.com/102173297/194731863-7a670561-8ccc-4ff2-9ddb-e1c1c54c75fa.jpg' alt="David Lowry sitting with his fiancee as she plays the video game he proposed to her with."/>
+        </a>
+        </div>
+        <a class='projectLink' href="https://github.com/Mersant/redesigned-barnacle" target="_blank" rel="noopener noreferrer">See this project on GitHub</a>
+
+
+        <div className='projectHeader'>
+          <a class='projectLink' href="https://github.com/Mersant/SuperTracker9000" target="_blank" rel="noopener noreferrer">
+            <img className='projectImage' src='https://user-images.githubusercontent.com/102173297/190265458-d3298f10-7a12-417a-bafd-8ea9cbdc69fb.png' alt="The SuperTracker9000 application running in a terminal window."/>
+          </a>
         <div className='projectInfo'>
           <h2>SuperTracker9000</h2>
           <p>
             A little JavaScript CLI app for managing a MySQL database.
           </p>
         </div>
-        <img className='projectImage' src='https://user-images.githubusercontent.com/102173297/190265458-d3298f10-7a12-417a-bafd-8ea9cbdc69fb.png'/>
-
         </div>
-        <a class='projectLink' href="https://www.google.com/" target="_blank" rel="noopener noreferrer">See this project on GitHub</a>
+        <a class='projectLink' href="https://github.com/Mersant/SuperTracker9000" target="_blank" rel="noopener noreferrer">See this project on GitHub</a>
 
 
         <div className='projectHeader'>
-        <img className='projectImage' src='https://user-images.githubusercontent.com/102173297/183931221-a54ebab6-b1ed-44d4-9df7-73d5a6c7d33d.png'/>
-
         <div className='projectInfo'>
-          <h2>CRPG Quiz</h2>
+          <h2>OSU AEV Project</h2>
           <p>
-            One of the very first websites I ever made, it may not be impressive but it was one of the more enjoyable projects I have worked on.
+            A school project I did at OSU during the spring, 2022 semeter. My group and I designed and tested an AEV. Following the project, we presented our findings to the class.
           </p>
         </div>
+        <a class='projectLink' href="https://github.com/Mersant/OSU-AEV-Project" target="_blank" rel="noopener noreferrer">
+          <img className='projectImage' src='https://user-images.githubusercontent.com/102173297/194734128-d736a841-7e91-4569-937b-43371be1f73b.jpg' alt="The roughly 1-foot long AEV scale model David and his team made for school."/>
+        </a>
         </div>
-        <a class='projectLink' href="https://www.google.com/" target="_blank" rel="noopener noreferrer">See this project on GitHub</a>
-
+        <a class='projectLink' href="https://github.com/Mersant/OSU-AEV-Project" target="_blank" rel="noopener noreferrer">See this project on GitHub</a>
 
         <div className='projectHeader'>
-        <img className='projectImage' src='https://github.com/meyerbw10/Find-My-Pets/blob/main/public/images/photo1.png?raw=true'/>
+        <a href="https://github.com/Mersant/Z80-Projects" target="_blank" rel="noopener noreferrer">
+        <img className='projectImage' src='https://github.com/Mersant/MyPortfolio/blob/main/assets/images/ZilogZ80.jpg?raw=true' alt="Close up of a Zilog Z80 as viewed through a magnifying glass."/>
+        </a>
         <div className='projectInfo'>
           <h2>Misc</h2>
           <p>
-            A few miscellaneous programs I have made in Z80 Assembly. 
+            Outside of web design, I enjoy computer hardware quite a bit. I have learned a bit of basic circuit design from books and used to carry around a briefcase when I was around 11-13 so I could draw computer schematics on the go. Tinkering with low-level programming languages like Assembly is something I also enjoy, and in this repo you can find some of the programs I have made.
           </p>
         </div>
         </div>
-        <a class='projectLink' href="https://www.google.com/" target="_blank" rel="noopener noreferrer">See this project on GitHub</a>
+        <a class='projectLink' href="https://github.com/Mersant/Z80-Projects" target="_blank" rel="noopener noreferrer">See this project on GitHub</a>
 
       </div>
     </section>
