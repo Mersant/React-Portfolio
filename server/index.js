605a58402b8be73f44696487da4bdb9ef3b47beb
Mersant <dlowry99@outlook.com>
2022-10-10 10:59:15 -0400

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,14 +1,17 @@
-const express = require('express')
- 
-const path = require('path')
- 
-const app = express()
- 
-const PORT = process.env.PORT || 3000
- 
-app.use(express.static(path.join(__dirname + "public")))
- 
-app.listen(PORT)
+const path = require('path');
+const express = require('express');
+const app = express();
+const publicPath = path.join(__dirname, '..', 'public');
+const port = process.env.PORT || 3000;
+
+app.use(express.static(publicPath));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(publicPath, 'index.html'));
+});
+
+app.listen(port, () => {
+  console.log(`Server now running on ${port}`);
+});
 
 /*// Server.js contains routes for the contact form on the "Get in touch" page. 
 
